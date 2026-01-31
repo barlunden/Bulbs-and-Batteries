0177b4724447b6caa666ed92d05a21c834d94ed2
Aage Barlund <barlund@gmail.com>
2026-01-31 21:27:37 +0100

    Fiks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import tailwindcss from '@tailwindcss/vite'; // Importer denne!
+import netlify from '@astrojs/netlify';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: netlify(),
   vite: {
-    plugins: [tailwindcss()], // Legg den til her
+    plugins: [tailwindcss()],
   },
 });
